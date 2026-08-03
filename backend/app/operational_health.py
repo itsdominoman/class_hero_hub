22b0cba94b3b1512d9aaca349701ae1e3b0e1a63
Dom <dom.dcubed@gmail.com>
2026-08-03 13:53:18 +0400

    Update readiness migration revision

--- backend/app/operational_health.py
+++ backend/app/operational_health.py
@@ -14,7 +14,7 @@ from .database import SessionLocal, engine, settings
 from .models_school import MessagingOperationsJob, MessagingWorkerHeartbeat, NotificationOutbox
 
 
-EXPECTED_MIGRATION_REVISION = "a0b1c2d3e4f5"
+EXPECTED_MIGRATION_REVISION = "a1e2f3c4d5b6"
 WORKER_STALE_SECONDS = 120
 QUEUE_BACKLOG_STALE_SECONDS = 300
 BACKUP_MAX_AGE_SECONDS = 30 * 3600
